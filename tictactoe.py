49018e38ffe80477818dc007a492453664f2d2fd
Math-ias <kools.m@northeastern.edu>
2021-04-07 23:58:12 -0400

    Rename data.py again, adjust examples, and generalize avail spots.
    This is not really an atomic commit.
    I was going through tictactoe.py adjusting comments.
    I noticed that I knew how to generalize available_spots,
    and from adjusting the examples I also started on
    adjusting asserts for other functions.
    
    Finally, I noticed the file could use another rename.
    The reasoning being that I figured it could made specific to
    tic-tac-toe, we plan on going after other examples.

--- tictactoe.py
+++ tictactoe.py
@@ -1,82 +1,48 @@
-# This file contains data definitions,
-# useful functions for tic-tac-toe games,
-# and tests for those useful functions as asserts.
+"""
+A module of useful functions for tic-tac-toe games
+"""
 
-# We use numpy for its definition of matrices. The slice syntax is invaluable.
+# Use this to manipulate tic-tac-toe data.
 import numpy as np
-# In order to define expected outputs easily, I don't want to write our coordinates manually.
+# Use this as a helper function for some tedious iterations.
 from itertools import product
-
-import examples
-
-# A tic-tac-toe mark is defined as one of the following.
-X = -1
-O = 1
-BLANK = 0
-
-# A game of tic tac toe is a matrix of N dimensions,
-# of the integer data type (same data type as the markers),
-# with each axis having a length of 3,
-# and all elements are either X, O, or BLANK.
-
-# For example, here we define a blank 2D game, and a blank 3D game.
-BLANK_GAME_2D = np.zeros((3, 3))
-BLANK_GAME_3D = np.zeros((3, 3, 3))
-# We define some more complicated examples below as well, the purpose being to aid testing.
-X_WON_2D = np.array([
-    [O, O, O],
-    [X, O, BLANK],
-    [X, X, X],
-])
-O_WON_2D = np.array([
-    [BLANK, BLANK, O],
-    [X, O, X],
-    [O, BLANK, BLANK],
-])
-TIED_2D = np.array([
-    [O, O, X],
-    [X, X, O],
-    [O, X, O],
-])
-MIDGAME_2D = np.array([
-    [O, BLANK, X],
-    [BLANK, BLANK, BLANK],
-    [X, BLANK, BLANK]
-])
-
-
-# A function to give all the possible actions for players in the game.
-def available_spots_3d(box):
-    """
-    Returns the available cubes for marks given some board as coordinate tuples.
-    :param: box   The 3d-tic-tac-toe structure.
-    :return:    An iterable of available cubes for marks.
-    """
-    to_return = []
-    for x in range(3):
-        for y in range(3):
-            for z in range(3):
-                if box[x][y][z] == BLANK:
-                    to_return.append((x, y, z))
-    return to_return
+# Use this to bring in data definitions, and some examples to test on.
+import tictactoedata as data
 
 
 # Documentation of product function.
 # "Cartesian product of input iterables.
 # For example, product(A, repeat=4) means the same as product(A, A, A, A)."
+# This is useful for available_spots, AND testing examples.
 
-# In our case, we use the product of the range [0,3) three times. It comes to us as a tuple, perfect!
-# We run a small test on our one example, all the coordinates should be returned.
-assert (set(available_spots_3d(BLANK_GAME_3D)) == set(product(range(3), repeat=3)))
+# A function to give all the possible actions for players in a game.
+def available_spots(game):
+    """
+    Returns the available spots for marks given some board as coordinate tuples.
+    :param: game    Any tic-tac-toe structure.
+    :return:        An iterable of available spots for marks.
+    """
+    return filter(lambda coordinate_tuple: game[coordinate_tuple] == data.BLANK_MARKER,
+                  product(range(3), repeat=game.ndim))
 
 
-# A helper function to determine if a player has won a 2D-game-slice of tic-tac-toe.
+# Sets are used to avoid order comparison.
+assert (set(available_spots(data.BLANK_GAME_2D)) == set(product(range(3), repeat=2)))
+assert (set(available_spots(data.BLANK_GAME_3D)) == set(product(range(3), repeat=3)))
+# We get no available spots in this finished game.
+assert (set(available_spots(data.TIED_2D)) == set())
+# We get some spots in MIDGAME_2D.
+# This is prone to break if the example changes.
+# Other example names are descriptive enough to ensure compat.
+assert (set(available_spots(data.MIDGAME_2D)) == set([(1, 0), (0, 1), (1, 1), (2, 1), (1, 2), (2, 2)]))
+
+
 def has_won_2d(board, player_mark):
     """
     Returns if the tic tac toe game has been won.
-    :param board:   The 2d-tic-tac-toe structure.
-    :param player_mark:  The mark to identify the player by.
-    :return:    True if the player has won the game, false otherwise.
+    :param board:       The 2d-tic-tac-toe structure.
+    :param player_mark: The mark to identify the player by.
+    :return:            True if the player has won the game, false otherwise.
     """
 
     # We check all the rows.
@@ -97,41 +63,51 @@ def has_won_2d(board, player_mark):
 
 
 # We test our function against our examples.
-assert (not has_won_2d(BLANK_GAME_2D, X))
-assert (not has_won_2d(BLANK_GAME_2D, O))
-assert (has_won_2d(X_WON_2D, X))
-assert (not has_won_2d(O_WON_2D, X))
-assert (has_won_2d(O_WON_2D, O))
-assert (not has_won_2d(O_WON_2D, X))
-assert (not has_won_2d(TIED_2D, X))
-assert (not has_won_2d(TIED_2D, O))
-assert (not has_won_2d(MIDGAME_2D, X))
-assert (not has_won_2d(MIDGAME_2D, O))
+# Neither player should win a blank game.
+assert (not has_won_2d(data.BLANK_GAME_2D, data.X_MARKER))
+assert (not has_won_2d(data.BLANK_GAME_2D, data.O_MARKER))
+# Only X should win the X_WON board.
+assert (has_won_2d(data.X_WON_2D, data.X_MARKER))
+assert (not has_won_2d(data.X_WON_2D, data.O_MARKER))
+# Only O should win the O_WON board.
+assert (has_won_2d(data.O_WON_2D, data.O_MARKER))
+assert (not has_won_2d(data.O_WON_2D, data.X_MARKER))
+# Neither should win a tied board.
+assert (not has_won_2d(data.TIED_2D, data.X_MARKER))
+assert (not has_won_2d(data.TIED_2D, data.O_MARKER))
+# Neither should win an in-progress-unfinished game.
+assert (not has_won_2d(data.MIDGAME_2D, data.X_MARKER))
+assert (not has_won_2d(data.MIDGAME_2D, data.O_MARKER))
 
 
-# A helper function to determine if a 2D-game-slice of tic-tac-toe is over.
 def game_over_2d(board):
     """
     Returns if the tic tac toe game is over.
     :param board:   The 2d-tic-tac-toe structure.
-    :return:        If the game is won (true), or tied by the two players. False otherwise.
+    :return:        True if the game is won or tied by players, False otherwise.s
     """
-    if has_won_2d(board, X):
+    if has_won_2d(board, data.X):
         return True
-    elif has_won_2d(board, O):
+    elif has_won_2d(board, data.O):
         return True
-    elif np.all(board != BLANK):
+    elif np.all(board != data.BLANK_MARKER):
         return True
     else:
         return False
 
 
-# Some small testing scenarios. Can obviously be less extensive than win checking.
-assert (not game_over_2d(BLANK_GAME_2D))
-assert (not game_over_2d(MIDGAME_2D))
-assert (game_over_2d(X_WON_2D))
-assert (game_over_2d(O_WON_2D))
-assert (game_over_2d(TIED_2D))
+# We test our function against our examples.
+# These tests should match the scenarios for has_won_2d.
+# A blank game is in-progress and not over.
+assert (not game_over_2d(data.BLANK_GAME_2D))
+# X has won, therefore the game is over.
+assert (game_over_2d(data.X_WON_2D))
+# O has won, therefore the game is over.
+assert (game_over_2d(data.O_WON_2D))
+# The game is tied, therefore the game is over.
+assert (game_over_2d(data.TIED_2D))
+# An in-progress game is not over.
+assert (not game_over_2d(data.MIDGAME_2D))
 
 
 def has_won_3d(box, player_mark):
@@ -174,15 +150,16 @@ def has_won_3d(box, player_mark):
         return True
     return False
 
-assert (has_won_3d(examples.X_WON_3D_X, X))
-assert (has_won_3d(examples.X_WON_3D_Y, X))
-assert (has_won_3d(examples.X_WON_3D_Z, X))
 
-assert (has_won_3d(examples.X_WON_3D_XY, X))
-assert (has_won_3d(examples.X_WON_3D_XZ, X))
-assert (has_won_3d(examples.X_WON_3D_YZ, X))
+assert (has_won_3d(data.X_WON_3D_X, data.X_MARKER))
+assert (has_won_3d(data.X_WON_3D_Y, data.X_MARKER))
+assert (has_won_3d(data.X_WON_3D_Z, data.X_MARKER))
 
-assert (has_won_3d(examples.X_WON_3D_XYZ, X))
+assert (has_won_3d(data.X_WON_3D_XY, data.X_MARKER))
+assert (has_won_3d(data.X_WON_3D_XZ, data.X_MARKER))
+assert (has_won_3d(data.X_WON_3D_YZ, data.X_MARKER))
+
+assert (has_won_3d(data.X_WON_3D_XYZ, data.X_MARKER))
 
 
 def game_over_3d(box):
@@ -191,11 +168,11 @@ def game_over_3d(box):
     :param box:   The 3d-tic-tac-toe structure.
     :return:        If the game is won (true), or tied by the two players. False otherwise.
     """
-    if has_won_3d(box, X):
+    if has_won_3d(box, data.X_MARKER):
         return True
-    elif has_won_3d(box, O):
+    elif has_won_3d(box, data.O_MARKER):
         return True
-    elif np.all(box != BLANK):
+    elif np.all(box != data.BLANK_MARKER):
         return True
     else:
         return False
